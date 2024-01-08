68ac992ddaa1a2422270b3a580a413413f1100f9
Karim <k.mehour@gmail.com>
2024-01-08 15:43:25 -0500

    Modify camera movekeybindings and constraint vertical cam rotation

--- src/hooks.c
+++ src/hooks.c
@@ -1,23 +1,17 @@
 #include "minirt.h"
 
-void ft_mouse_select(void *param)
+t_vec3 ft_constrain_vertical_rot(t_cam *cam, t_vec3 axis, double theta)
 {
-	t_data *data;
-	mlx_t *mlx;
-	t_ray ray;
-	int32_t mousePos[2];
+	t_vec3 new_dir;
+	const double THRESH = 0.99;
+	double likeness;
 
-	data = (t_data *) param;
-	mlx = data->mlx;
-	if (mlx_is_mouse_down(mlx, MLX_MOUSE_BUTTON_LEFT))
-	{
-		mlx_get_mouse_pos(mlx, mousePos, mousePos + 1);
-		ray = ft_compute_ray(data->cam, mousePos[1], mousePos[0]);
-		ft_print_ray(ray);
-		t_hit_point hit_pt = ft_get_closest_hitpoint(data->obj, data->object_count, ray);
-		ft_print_hitpt(hit_pt);
-		printf("\n\n");
-	}
+	new_dir = ft_rodrigues_rotation(cam->dir, axis, theta);
+	likeness = fabs(ft_vec3_dot(new_dir, cam->vup));
+	if (likeness > THRESH)
+		return (cam->dir);
+
+	return (new_dir);
 }
 
 void ft_nav_cam_rotation(mlx_t *mlx, t_data *data, t_cam *cam)
@@ -25,9 +19,12 @@ void ft_nav_cam_rotation(mlx_t *mlx, t_data *data, t_cam *cam)
 	double theta;
 	theta = ft_deg_to_rad(5);
 	t_vec3 axis;
+	const double EPS = 0.005;
+	t_vec3 tmp;
+
+	axis = cam->vup;
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
 	{
-		axis = cam->vup;
 		cam->dir = ft_rodrigues_rotation(cam->dir, axis, -theta);
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
@@ -35,17 +32,18 @@ void ft_nav_cam_rotation(mlx_t *mlx, t_data *data, t_cam *cam)
 		axis = cam->vup;
 		cam->dir = ft_rodrigues_rotation(cam->dir, axis, theta);
 	}
+
+	axis = ft_vec3_cross_prod(cam->dir, cam->vup);
 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
 	{
 		// TODO: limit up/down view range
-		axis = ft_vec3_cross_prod(cam->dir, cam->vup);
-		cam->dir = ft_rodrigues_rotation(cam->dir, axis, -theta);
+		cam->dir = ft_constrain_vertical_rot(cam, axis, -theta);
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
 	{
 		// TODO: limit up/down view range
-		axis = ft_vec3_cross_prod(cam->dir, cam->vup);
-		cam->dir = ft_rodrigues_rotation(cam->dir, axis, theta);
+		tmp = cam->dir;
+		cam->dir = ft_constrain_vertical_rot(cam, axis, theta);
 	}
 }
 
@@ -56,31 +54,44 @@ void ft_nav_cam_translation(mlx_t *mlx, t_data *data, t_cam *cam)
 
 	// Translate Camera
 	// move left
-	if (mlx_is_key_down(mlx, MLX_KEY_H))
+	if (mlx_is_key_down(mlx, MLX_KEY_KP_4))
 	{
 		axis = ft_vec3_cross_prod(cam->dir, cam->vup);
 		axis = ft_vec3_scal_prod(axis, 4 * EPS);
 		cam->ori = ft_vec3_minus(cam->ori, axis);
 	}
 	// move right
-	if (mlx_is_key_down(mlx, MLX_KEY_L))
+	if (mlx_is_key_down(mlx, MLX_KEY_KP_6))
 	{
 		axis = ft_vec3_cross_prod(cam->dir, cam->vup);
 		axis = ft_vec3_scal_prod(axis, 4 * EPS);
 		cam->ori = ft_vec3_add(cam->ori, axis);
 	}
 	// move up
-	if (mlx_is_key_down(mlx, MLX_KEY_K))
+	if (mlx_is_key_down(mlx, MLX_KEY_KP_9))
 	{
 		axis = ft_vec3_scal_prod(cam->vup, 4 * EPS);
 		cam->ori = ft_vec3_add(cam->ori, axis);
 	}
 	// move down
-	if (mlx_is_key_down(mlx, MLX_KEY_J))
+	if (mlx_is_key_down(mlx, MLX_KEY_KP_7))
 	{
 		axis = ft_vec3_scal_prod(cam->vup, 4 * EPS);
 		cam->ori = ft_vec3_minus(cam->ori, axis);
 	}
+	// move forward
+	if (mlx_is_key_down(mlx, MLX_KEY_KP_8))
+	{
+		axis = ft_vec3_scal_prod(cam->dir, 4 * EPS);
+		cam->ori = ft_vec3_add(cam->ori, axis);
+	}
+	// move backward
+	if (mlx_is_key_down(mlx, MLX_KEY_KP_5))
+	{
+		axis = ft_vec3_scal_prod(cam->dir, 4 * EPS);
+		cam->ori = ft_vec3_minus(cam->ori, axis);
+	}
+
 }
 
 /*
@@ -93,7 +104,7 @@ void ft_nav_hook(void *param)
 	t_vec3 axis;
 	t_cam *cam;
 
-	data = (t_data*) param;
+	data = (t_data *)param;
 	cam = &data->cam;
 	mlx = data->mlx;
 	// Quit minirt
@@ -117,7 +128,6 @@ void ft_nav_hook(void *param)
 	// Camera Translation
 	ft_nav_cam_translation(mlx, data, cam);
 }
-
 
 /*
 	brief: mlx loop hook for rendering
